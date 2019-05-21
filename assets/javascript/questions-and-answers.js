f9f056a7ca3c5209f51339d14fd4314089e44720
Hibi <correiaph13@gmail.com>
2019-05-21 15:29:30 -0500

    file with questions/answers completed

--- assets/javascript/questions-and-answers.js
+++ assets/javascript/questions-and-answers.js
@@ -1,19 +1,89 @@
-// create objects based on questions and answers
+// create objects for on questions and answers
 
-const questions=[
+const questions = [
     {
-      Q1: "what is Neo's matrix name?",
-      aq1answers: {
+        Q1: "what is Neo's matrix name?",
+        q1answers: {
                 A: "John P. Black",
                 B: "Timothy J. Washington",
                 C: "Thomas A. Anderson",
                 D: "James L. Burns",
-      }
+                }
     },{
         Q2: "What is the name of Morpheus's ship?",
-      aq1answers: {
+        q2Answers: {
                 A: "The Enterprise",
                 B: "The Nebuchadnezzar",
                 C: "The Nautilus",
                 D: "The Millenium Falcon",
-    }
+                }
+    },{
+        Q3: "What is the phrase written above the door in the Oracle's kitchen?",
+        q3Answers: {
+                A: "Caveat Emptor",
+                B: "Temet Nosce",
+                C: "Veni Vidi Vici",
+                D: "Mihi Mater Piscis Est",
+                }
+    },{
+        Q4: "What is the name of the program that protects The Oracle?",
+        q4Answers: {
+                A: "Seraph",
+                B: "Windows Defender",
+                C: "Achilles",
+                D: "Cerberus",
+                }
+
+    },{ 
+        Q5: "What clue does Morpheus tell Neo to follow first?",
+        q5Answers: {
+                A: "The Signs",
+                B: "His Heart",
+                C: "The Yellow Brick Road",
+                D: "The White Rabbit",
+                }
+
+    
+    },{ 
+        Q6: "What is the name of the last city of humanity?",
+        q6Answers: {
+                A: "Terra Nova",
+                B: "Zion",
+                C: "Eden",
+                D: "New New York",
+                }
+
+    },{ Q7: "Who designed the Woman in the red dress program?"
+        q7Answers: {
+                A: "Cypher",
+                B: "The Architect",
+                C: "Trinity",
+                D: "Mouse",
+                }
+
+    },{ Q8: "How did Trinity know Neo was the one?",
+        q8Answers: {
+                A: "She had a vision",
+                B: "The Architect told her",
+                C: "She loves him",
+                D: "She finds it in the matrix code",
+                }
+
+    },{ Q9: "What is the name of the Merovingian's wife?"
+        q9Answers: {
+                A: "Persephone",
+                B: "Athena",
+                C: "Aphrodite",
+                D: "Hera",
+                }
+
+    },{ Q10: "What iteration of the Matrix does the Architect tell Neo they are on?"
+        q10Answers: {
+                A: "8",
+                B: "6",
+                C: "25",
+                D: "10",
+                }
+
+    
+    }]
