1e3a2014341dc7a4aa8516062d299f16b9ecb393
Hibi <correiaph13@gmail.com>
2019-05-21 14:48:24 -0500

    added separate file for questions and answers objects

---
+++ assets/javascript/questions-and-answers.js
@@ -0,0 +1,19 @@
+// create objects based on questions and answers
+
+const questions=[
+    {
+      Q1: "what is Neo's matrix name?",
+      aq1answers: {
+                A: "John P. Black",
+                B: "Timothy J. Washington",
+                C: "Thomas A. Anderson",
+                D: "James L. Burns",
+      }
+    },{
+        Q2: "What is the name of Morpheus's ship?",
+      aq1answers: {
+                A: "The Enterprise",
+                B: "The Nebuchadnezzar",
+                C: "The Nautilus",
+                D: "The Millenium Falcon",
+    }
