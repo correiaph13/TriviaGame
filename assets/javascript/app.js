3765a6d7b3ec59cf1218fa5a2581fc46275393f3
Hibi <correiaph13@gmail.com>
2019-05-27 19:53:42 -0500

    added radio buttons and divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,33 +2,57 @@
 $(document).ready(function() {
     
 function questionLayout () {
+
     //for every i (question) create a div containing it
+
     for (i=0; i<questions.length; i++){
+
     var newDivQ = $("<div class = 'questionContainer'>").text(questions[i].Q);
-    var newDivA = $("<button class = 'answerA'>").text(questions[i].qAnswers.A);
-    var newDivB = $("<button class = 'answerB'>").text(questions[i].qAnswers.B); 
-    var newDivC = $("<button class = 'answerC'>").text(questions[i].qAnswers.C);
-    var newDivD = $("<button class = 'answerD'>").text(questions[i].qAnswers.D);
+
+
+        var newDivA = $("<input type='radio' class = 'answerA'>");
+        var newDivAT = $("<div class='answerContainer'>").text(questions[i].qAnswers.A);
+        
+        var newDivB = $("<input type='radio' class = 'answerB'>"); 
+        var newDivBT = $("<div class='answerContainer'>").text(questions[i].qAnswers.B);
     
-    $(newDivQ).append(newDivA);
-    $(newDivQ).append(newDivB);
-    $(newDivQ).append(newDivC);
-    $(newDivQ).append(newDivD);
+        var newDivC = $("<input type='radio' class = 'answerC'>"); 
+        var newDivCT = $("<div class='answerContainer'>").text(questions[i].qAnswers.C);
+
+        var newDivD = $("<input type='radio' class = 'answerD'>"); 
+        var newDivDT = $("<div class='answerContainer'>").text(questions[i].qAnswers.D);
+    
+    $(newDivQ).append(newDivAT);
+    $(newDivAT).prepend(newDivA);
+   
+
+    $(newDivQ).append(newDivBT);
+    $(newDivBT).prepend(newDivB);
+
+    
+    $(newDivQ).append(newDivCT);
+    $(newDivCT).prepend(newDivC);
+
+    
+    $(newDivQ).append(newDivDT);
+    $(newDivDT).prepend(newDivD);
+
+ 
     
     $('#questionsForm').append(newDivQ);
 
-    var correctAnswers = [questions[i].correctA];
+    // var correctAnswers = [questions[i].correctA];
 
 }   
 }
 questionLayout();
-var userAnswers = [];
+// var userAnswers = [];
 
-$("button").on("click", function(event){
-    var userInput = $(this).attr("class");
-    userAnswers.push(userInput);
-    console.log(userAnswers);
+// $("button").on("click", function(event){
+//     var userInput = $(this).attr("class");
+//     userAnswers.push(userInput);
+//     console.log(userAnswers);
 
-    }); 
+//     }); 
 
 });
