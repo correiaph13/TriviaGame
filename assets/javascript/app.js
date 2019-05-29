1cf7292c9a62cf0933e35ec2d5f09a940667eb17
Hibi <correiaph13@gmail.com>
2019-05-29 11:13:53 -0500

    new comments explaining code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,65 +4,65 @@ $(document).ready(function() {
 
 
 
-function questionLayout () {
+    function questionLayout () {
 
-    //for every i (question) create a div containing it
+        //for every i (question) create a div containing it, used tilde to be able to use the vartiable inside the newDivA 
+        // string to iterate the radio names in order to separate the groups of answers for each question
+        // to each question div, i appended the text for the answers and prepended a radio button to the text
 
-    for (i=0; i<questions.length; i++){
+        for (i=0; i<questions.length; i++){
 
+            var newDivQ = $("<div class = 'questionContainer'>").text(questions[i].Q);
+            
+            var radioName = ('answer'+[i]);
 
+                var newDivA = $(`<input type='radio' name=radioName${i}  class = 'answerA'>`);
+                newDivA.attr('value', questions[i].qAnswers.A);
+                var newDivAT = $("<div class='answerContainer'>").text(questions[i].qAnswers.A);
+                
+                var newDivB = $(`<input type='radio' name=radioName${i}  class = 'answerB'>`);
+                newDivB.attr('value', questions[i].qAnswers.A); 
+                var newDivBT = $("<div class='answerContainer'>").text(questions[i].qAnswers.B);
+            
+                var newDivC = $(`<input type='radio' name=radioName${i}  class = 'answerC'>`); 
+                newDivC.attr('value', questions[i].qAnswers.A);
+                var newDivCT = $("<div class='answerContainer'>").text(questions[i].qAnswers.C);
 
-
-    var newDivQ = $("<div class = 'questionContainer'>").text(questions[i].Q);
-    
-    var radioName = ('answer'+[i]);
-
-        var newDivA = $(`<input type='radio' name=radioName${i}  class = 'answerA'>`);
-        newDivA.attr('value', questions[i].qAnswers.A);
-        var newDivAT = $("<div class='answerContainer'>").text(questions[i].qAnswers.A);
+                var newDivD = $(`<input type='radio' name=radioName${i}  class = 'answerD'>`); 
+                newDivD.attr('value', questions[i].qAnswers.A);
+                var newDivDT = $("<div class='answerContainer'>").text(questions[i].qAnswers.D);
         
-        var newDivB = $(`<input type='radio' name=radioName${i}  class = 'answerB'>`);
-        newDivB.attr('value', questions[i].qAnswers.A); 
-        var newDivBT = $("<div class='answerContainer'>").text(questions[i].qAnswers.B);
-    
-        var newDivC = $(`<input type='radio' name=radioName${i}  class = 'answerC'>`); 
-        newDivC.attr('value', questions[i].qAnswers.A);
-        var newDivCT = $("<div class='answerContainer'>").text(questions[i].qAnswers.C);
+                $(newDivQ).append(newDivAT);
+                $(newDivAT).prepend(newDivA);
+            
 
-        var newDivD = $(`<input type='radio' name=radioName${i}  class = 'answerD'>`); 
-        newDivD.attr('value', questions[i].qAnswers.A);
-        var newDivDT = $("<div class='answerContainer'>").text(questions[i].qAnswers.D);
-    
-    $(newDivQ).append(newDivAT);
-    $(newDivAT).prepend(newDivA);
-   
+                $(newDivQ).append(newDivBT);
+                $(newDivBT).prepend(newDivB);
 
-    $(newDivQ).append(newDivBT);
-    $(newDivBT).prepend(newDivB);
+                
+                $(newDivQ).append(newDivCT);
+                $(newDivCT).prepend(newDivC);
 
-    
-    $(newDivQ).append(newDivCT);
-    $(newDivCT).prepend(newDivC);
+                
+                $(newDivQ).append(newDivDT);
+                $(newDivDT).prepend(newDivD);
 
-    
-    $(newDivQ).append(newDivDT);
-    $(newDivDT).prepend(newDivD);
+                $('#questionsForm').append(newDivQ);
 
-    $('#questionsForm').append(newDivQ);
+            // ============================================================================================ //
 
     var correctA = [questions[i].correctA]
     // console.log(correctA);
 
 $("#userAnswers").on("click", function(event){
     var userInput = $("input[type='radio']:checked");
-    var correctAnswers = 0;
     
 
     if (userInput.length < 10) {
         alert ("Please answer all the questions first!");
     }
     else {
-        
+        var correctAnswers = 0;
         for (let j = 0; j < userInput.length; j++) {
             if (correctA[j] === userInput[j].value) {
                 correctAnswers++;
